ebe9d9aca14d1d4ab2f1ed783980911292dec64f
SuperMaddie <mahdiye.jamali@gmail.com>
2015-05-22 20:51:14 -0700

    Sensor Added
    Sensor file is added

--- src/usecase/UseCaseMain.java
+++ src/usecase/UseCaseMain.java
@@ -4,32 +4,30 @@ import generic.RoverThreadHandler;
 
 import java.io.IOException;
 
-public class UseCaseMain
-{
+public class UseCaseMain {
 
-  public static void main(String[] args)
-  {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int noOfSensors = 5;
+		int port = 9897;
 
-	//Server port
-	int port = 9897;
+		try {
 
-	try
-	{
-	  UseCaseServer useCaseServer = new UseCaseServer(port);
-	  Thread server = RoverThreadHandler.getRoverThreadHandler().getNewThread(useCaseServer);
+			UseCaseServer useCaseServer = new UseCaseServer(port);
+			Thread server = RoverThreadHandler.getRoverThreadHandler().getNewThread(useCaseServer);
 
-	  UseCaseClient useCaseClient = new UseCaseClient(port, null);
-	  Thread client = RoverThreadHandler.getRoverThreadHandler().getNewThread(useCaseClient);
+			server.start();
+			for (int i = 0; i < noOfSensors; i++) {
+				Sensor sensor = new Sensor(port, null, i + 1);
+				Thread client = RoverThreadHandler.getRoverThreadHandler().getNewThread(sensor);
+				client.start();
+			}
 
-	  server.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-	  client.start();
-
-	} catch (IOException e)
-	{
-	  e.printStackTrace();
 	}
-
-  }
 
 }
